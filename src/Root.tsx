760af6db4585216ac8614e9f537ab12288b65fda
pawgog <pgogola10@gmail.com>
2021-10-16 16:09:42 +0200

    Set up React app with styling env.

---
+++ src/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './styles/Root.scss';
+
+function Root() {
+  return (
+    <div className="dashboard-body">
+      test
+    </div>
+  );
+}
+
+export default Root;
