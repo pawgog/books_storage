be7584359ecd36db49e44c434a8e7b68e4b6b4d4
pawgog <pgogola10@gmail.com>
2021-10-27 23:27:30 +0200

    Improvements on error handling in formik.

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,46 +1,34 @@
 import * as React from 'react';
+import * as Yup from 'yup';
 import { Formik } from 'formik';
 
 const BookForm = () => (
-    <div>
-        <Formik
-            initialValues={{ author: '', title: '' }}
-            validate={(values) => {
-                const errors = { author: '', title: '' };
-                if (!values.author) {
-                    errors.author = 'Author name is required';
-                } else if (!values.title) {
-                    errors.title = 'Book title is required';
-                }
-                return errors;
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    // eslint-disable-next-line no-alert
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400);
-            }}
-        >
-            {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        name="author"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.author}
-                    />
-                    {errors.author && touched.author}
-                    <input type="title" name="title" onChange={handleChange} onBlur={handleBlur} value={values.title} />
-                    {errors.title && touched.title}
-                    <button type="submit" disabled={isSubmitting}>
-                        Submit
-                    </button>
-                </form>
-            )}
-        </Formik>
-    </div>
+    <Formik
+        initialValues={{ author: '', title: '' }}
+        validationSchema={Yup.object().shape({
+            author: Yup.string().required('Required'),
+            title: Yup.string().required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+                // eslint-disable-next-line no-alert
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+            }, 400);
+        }}
+    >
+        {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="author" onChange={handleChange} onBlur={handleBlur} value={values.author} />
+                {errors.author && touched.author}
+                <input type="title" name="title" onChange={handleChange} onBlur={handleBlur} value={values.title} />
+                {errors.title && touched.title}
+                <button type="submit" disabled={isSubmitting}>
+                    Submit
+                </button>
+            </form>
+        )}
+    </Formik>
 );
 
 export default BookForm;
