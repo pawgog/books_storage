4f543b2aa331470bdb1475325876d9c2422596ee
pawgog <pgogola10@gmail.com>
2021-12-06 23:21:17 +0100

    Fetch data after fill search input.

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -3,13 +3,26 @@ import { Autocomplete, TextField, IconButton } from '@mui/material';
 import ManageSearch from '@mui/icons-material/ManageSearch';
 import { getBooksFetching } from '../helpers/fetchData';
 
+export interface BookObject {
+    id: string;
+    book: string;
+}
+
 function BookSearch() {
     const [searchValue, setSearchValue] = useState('');
-    const [searchBooksCollect, setSearchBooksCollect] = useState<Array<string>>(['']);
+    const [searchBooksCollect, setSearchBooksCollect] = useState<Array<BookObject>>([{ id: '', book: '' }]);
 
     useEffect(() => {
         getBooksFetching(searchValue)
-            .then(({ items }) => setSearchBooksCollect(items))
+            .then(({ items }) => {
+                const selectItems = items.map(
+                    (item: { id: string, volumeInfo: { authors: string, title: string } }) => ({
+                        id: item.id,
+                        book: `${item.volumeInfo.authors[0]} - ${item.volumeInfo.title}`,
+                    }),
+                );
+                setSearchBooksCollect(selectItems);
+            })
             .catch((error) => {
                 // eslint-disable-next-line no-console
                 console.log(error);
@@ -24,6 +37,7 @@ function BookSearch() {
             <Autocomplete
                 freeSolo
                 id="books-search"
+                options={searchBooksCollect.map((option) => option.book)}
                 onInputChange={(e, inputValue) => {
                     setSearchValue(inputValue);
                 }}
