c0c410ad3df7d8cfa73fc5f1fa9743684df252d8
pawgog <pgogola10@gmail.com>
2021-12-15 20:32:58 +0100

    Search input improvements.

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Autocomplete, TextField, IconButton } from '@mui/material';
+import { Autocomplete, MenuItem, TextField, IconButton } from '@mui/material';
 import ManageSearch from '@mui/icons-material/ManageSearch';
 import { getBooksFetching } from '../helpers/fetchData';
 
@@ -10,29 +10,37 @@ export interface BookObject {
 
 function BookSearch({ getBookDetails }: any) {
     const [searchValue, setSearchValue] = useState('');
+    const [openSuggestions, setOpenSuggestions] = useState(false);
     const [searchBooksAllCollect, setSearchBooksAllCollect] = useState<any>([{}]);
     const [searchBooksCollect, setSearchBooksCollect] = useState<Array<BookObject>>([{ id: '', book: '' }]);
 
     useEffect(() => {
-        getBooksFetching(searchValue)
-            .then(({ items }) => {
-                setSearchBooksAllCollect(items);
-                const selectItems = items.map(
-                    (item: { id: string, volumeInfo: { authors: string, title: string } }) => ({
-                        id: item.id,
-                        book: `${item.volumeInfo?.authors[0]} - ${item.volumeInfo.title}`,
-                    }),
-                );
-                setSearchBooksCollect(selectItems);
-            })
-            .catch((error) => {
-                // eslint-disable-next-line no-console
-                console.log(error);
-            });
+        if (searchValue) {
+            getBooksFetching(searchValue)
+                .then(({ items }) => {
+                    setSearchBooksAllCollect(items);
+                    const selectItems = items.map(
+                        (item: { id: string, volumeInfo: { authors: string, title: string } }) => ({
+                            id: item.id,
+                            book: `${
+                                typeof item.volumeInfo?.authors !== 'undefined'
+                                    ? `${item.volumeInfo?.authors[0]} - `
+                                    : ''
+                            }${item.volumeInfo?.title}`,
+                        }),
+                    );
+                    setSearchBooksCollect(selectItems);
+                })
+                .catch((error) => {
+                    // eslint-disable-next-line no-console
+                    console.log(error);
+                });
+        }
     }, [searchValue]);
 
     const searchSelected = (selected: any) => {
-        const selectedBookDetails = searchBooksCollect.find((item) => item.book === selected.value);
+        setOpenSuggestions(false);
+        const selectedBookDetails = searchBooksCollect.find((item) => item.book === selected);
         const bookAllDetails = searchBooksAllCollect?.find(
             (book: { id: string }) => book.id === selectedBookDetails?.id,
         );
@@ -52,8 +60,14 @@ function BookSearch({ getBookDetails }: any) {
                 options={searchBooksCollect.map((option) => option.book)}
                 onInputChange={(e, inputValue) => {
                     setSearchValue(inputValue);
+                    setOpenSuggestions(true);
                 }}
-                onSelect={(event) => searchSelected(event.target)}
+                open={openSuggestions}
+                renderOption={(props: any) => (
+                    <MenuItem key={props.id} value={props.key} onClick={() => searchSelected(props.key)}>
+                        {props.key}
+                    </MenuItem>
+                )}
                 renderInput={(params: any) => <TextField {...params} label="Search books" />}
             />
             <IconButton aria-label="search" color="primary">
